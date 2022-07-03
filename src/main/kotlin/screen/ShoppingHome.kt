a9453a6ec0620bb0c895834faac70c3db6899727
Jina <chjina0131@gmail.com>
2022-07-03 12:35:47 +0900

    Home화면과 Category 화면으로 분리

--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -8,30 +8,8 @@ class shoppingHome {
     }
 
     private fun showCategories() {
-        /*
-        실습 2
-        1. 상품 카테고리 표기
-        2. 사용자 입력 받기
-        3. 사용자가 기대하는 값을 입력하지 않는 경우에 대한 처리
-         */
-        val categories = arrayOf("패션", "전자기기", "반려동물용품")
-        for (category in categories) {
-            println(category)
-        }
-        println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
-
-        var selectedCategory = readLine()
-
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 입력해주세요")
-            selectedCategory = readLine()
-        }
-        if (selectedCategory == "#") {
-            //TODO 1. 장바구니 이동
-        } else {
-            //TODO 2. 카테고리 상품 목록 보여주기
-            //TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
-        }
+        val shoppingCategory = shoppingCategory()
+        shoppingCategory.showCategories()
     }
 
     private fun showWelcomeMassage() {
